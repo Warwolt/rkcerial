106cf60fb9bcf1aad42880b9009f8b8fcf754751
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-16 01:43:15 +0100

    Send current time to arduino

--- monitor.py
+++ monitor.py
@@ -1,8 +1,10 @@
 import sys
 import serial
 import signal
+from datetime import datetime
 
 def signal_handler(sig, frame):
+    print("[Monitor] serial port monitor closed")
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -16,6 +18,26 @@ if len(sys.argv) < 2:
 baud = 9600
 port = sys.argv[1]
 ser = serial.Serial(port, baud)
+# We use a quirke where opening serial communication with the Arduino Uno will
+# reset it, and therefore send the time stamp message after we receive the first
+# message from the Arduino.
+has_sent_time = False
+
+print("[Monitor] serial port monitor opened (close with ctrl+C)")
+
 while True:
+    # Wait until receive a line
     ser_in = ser.readline().decode('utf-8').strip('\n')
     print(ser_in)
+
+    # If not yet sent current time, send it
+    if not has_sent_time:
+        has_sent_time = True
+        current_time = datetime.now()
+        time_str = "TIME {} {} {} {}".format(
+            current_time.hour,
+            current_time.minute,
+            current_time.minute,
+            current_time.microsecond // 1000,
+        )
+        ser.write(time_str.encode())
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <avr/io.h>
-#include <util/delay.h>
 #include <arduino/USBAPI.h>
+#include <avr/io.h>
 #include <stdio.h>
+#include <util/delay.h>
 
 // from wiring.c
 void init(void);
@@ -16,7 +16,20 @@ int main(void) {
 
 	Serial.print("Program Start\n");
 
+	char buffer[64] = { 0 };
+	int buffer_len = 0;
 	while (1) {
+		while (Serial.available() > 0 && buffer_len < (64 - 2)) {
+			buffer[buffer_len++] = Serial.read();
+		}
+
+		if (buffer_len > 0) {
+			buffer[buffer_len++] = '\n';
+			buffer[buffer_len++] = '\0';
+			Serial.print(buffer);
+			buffer_len = 0;
+		}
+
 		PORTB = 0b00100000; // PB5
 		_delay_ms(1000);
 
