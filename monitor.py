85f7ccac1146d5179ede200049a8f51a88909d07
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-16 03:03:00 +0100

    Send timestamp and prefix prints with it

--- monitor.py
+++ monitor.py
@@ -3,9 +3,24 @@ import serial
 import signal
 from datetime import datetime
 
+
+def timestamp_now():
+    '''
+    Returns the current time as milliseconds since 00:00
+    '''
+    current_time = datetime.now()
+    now_ms = 0
+    now_ms += current_time.hour * (1000 * 60 * 60)
+    now_ms += current_time.minute * (1000 * 60)
+    now_ms += current_time.second * 1000
+    now_ms += current_time.microsecond // 1000
+    return  now_ms
+
+
 def signal_handler(sig, frame):
     print("[Monitor] serial port monitor closed")
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -18,7 +33,7 @@ if len(sys.argv) < 2:
 baud = 9600
 port = sys.argv[1]
 ser = serial.Serial(port, baud)
-# We use a quirke where opening serial communication with the Arduino Uno will
+# We use a quirk where opening serial communication with the Arduino Uno will
 # reset it, and therefore send the time stamp message after we receive the first
 # message from the Arduino.
 has_sent_time = False
@@ -33,11 +48,6 @@ while True:
     # If not yet sent current time, send it
     if not has_sent_time:
         has_sent_time = True
-        current_time = datetime.now()
-        time_str = "TIME {} {} {} {}".format(
-            current_time.hour,
-            current_time.minute,
-            current_time.minute,
-            current_time.microsecond // 1000,
-        )
+        time_str = "TIMENOW {}".format(timestamp_now())
+        print("[Monitor]", time_str)
         ser.write(time_str.encode())
--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,72 @@
 void init(void);
 unsigned long millis(void);
 
+static bool string_starts_with(const char* str, const char* prefix) {
+	return strncmp(prefix, str, strlen(prefix)) == 0;
+}
+
+static unsigned long try_get_timestamp(void) {
+	unsigned long timestamp_ms = 0;
+
+	char input_buf[64] = { 0 };
+	int input_buf_len = 0;
+	while (Serial.available() > 0 && input_buf_len < (64 - 2)) {
+		input_buf[input_buf_len++] = Serial.read();
+	}
+
+	if (input_buf_len > 0) {
+		input_buf[input_buf_len++] = '\n';
+		input_buf[input_buf_len++] = '\0';
+		input_buf_len = 0;
+
+		if (string_starts_with(input_buf, "TIMENOW")) {
+			int offset = strlen("TIMENOW ");
+			timestamp_ms = strtol(&input_buf[offset], NULL, 10);
+			Serial.print(timestamp_ms);
+		}
+	}
+
+	return timestamp_ms;
+}
+
+static void print_time_now(unsigned long timestamp_ms) {
+	if (timestamp_ms == 0) {
+		return;
+	}
+
+	char now_str[64] = { 0 };
+
+	// unsigned long now_ms = timestamp_ms + millis();
+	unsigned long now_ms = timestamp_ms;
+
+	unsigned long hour = (now_ms / (1000L * 60L * 60L)) % 24L;
+	unsigned long minutes = (now_ms / (1000L * 60L)) % 60L;
+	unsigned long seconds = (now_ms / 1000L) % 60L;
+	unsigned long milliseconds = now_ms % 1000L;
+	snprintf(now_str, 64, "[%lu:%lu:%lu:%lu] ", hour, minutes, seconds, milliseconds);
+
+	Serial.print(now_str);
+}
+
 int main(void) {
 	init();
 
 	DDRB = 0b00100000; // PB5 output
 
 	Serial.begin(9600);
-
 	Serial.print("Program Start\n");
 
-	char buffer[64] = { 0 };
-	int buffer_len = 0;
+	unsigned long timestamp_ms = 0;
 	while (1) {
-		while (Serial.available() > 0 && buffer_len < (64 - 2)) {
-			buffer[buffer_len++] = Serial.read();
-		}
-
-		if (buffer_len > 0) {
-			buffer[buffer_len++] = '\n';
-			buffer[buffer_len++] = '\0';
-			Serial.print(buffer);
-			buffer_len = 0;
+		if (timestamp_ms == 0) {
+			timestamp_ms = try_get_timestamp();
 		}
 
 		PORTB = 0b00100000; // PB5
 		_delay_ms(1000);
 
+		print_time_now(timestamp_ms);
 		Serial.print("Hello World\n");
-		Serial.println(millis());
 
 		PORTB = 0b00000000; // PB5
 		_delay_ms(1000);
