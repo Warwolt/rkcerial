4c7df2aebd745c9315f233c6b26ed3a60748a512
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-16 14:35:18 +0100

    Put script inside if main

--- monitor.py
+++ monitor.py
@@ -14,7 +14,7 @@ def timestamp_now():
     now_ms += current_time.minute * 1000 * 60
     now_ms += current_time.second * 1000
     now_ms += current_time.microsecond // 1000
-    return  now_ms
+    return now_ms
 
 
 def signal_handler(sig, frame):
@@ -22,31 +22,32 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-signal.signal(signal.SIGINT, signal_handler)
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
 
-if len(sys.argv) < 2:
-    print("Usage: monitor.py PORT")
-    print("")
-    print("PORT should be the serial port name on your OS, e.g. COM2 on Windows or ttyS0 on Linux")
-    exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: monitor.py PORT", end="")
+        print("")
+        print("PORT should be the serial port name on your OS, e.g. COM2 on Windows or ttyS0 on Linux")
+        exit(1)
 
-baud = 9600
-port = sys.argv[1]
-ser = serial.Serial(port, baud)
-# We use a quirk where opening serial communication with the Arduino Uno will
-# reset it, and therefore send the time stamp message after we receive the first
-# message from the Arduino.
-has_sent_time = False
+    baud = 9600
+    port = sys.argv[1]
+    ser = serial.Serial(port, baud)
+    # We use a quirk where opening serial communication with the Arduino Uno will
+    # reset it, and therefore send the time stamp message after we receive the first
+    # message from the Arduino.
+    has_sent_time = False
 
-print("[Monitor] serial port monitor opened (close with ctrl+C)")
+    print("[Monitor] serial port monitor opened (close with Ctrl+C)")
 
-while True:
-    # Wait until receive a line
-    ser_in = ser.readline().decode('utf-8').strip('\n')
-    print(ser_in)
+    while True:
+        # Wait until receive a line
+        ser_in = ser.readline().decode('utf-8').strip('\n')
+        print(ser_in)
 
-    # If not yet sent current time, send it
-    if not has_sent_time:
-        has_sent_time = True
-        time_str = "TIMENOW {}".format(timestamp_now())
-        ser.write(time_str.encode())
+        # If not yet sent current time, send it
+        if not has_sent_time:
+            has_sent_time = True
+            time_str = "TIMENOW {}".format(timestamp_now())
+            ser.write(time_str.encode())
